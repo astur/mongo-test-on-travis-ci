74d93e84719e513174fa818f8e8e090df954ffb7
astur <astur@yandex.ru>
2018-02-20 18:34:30 +0300

    stub of test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const test = require('ava');
-const mongo-test-on-travis-ci = require('.');
+const db = require('.');
 
 test('mongo-test-on-travis-ci', t => {
     t.true(true);
-    t.is(mongo-test-on-travis-ci, mongo-test-on-travis-ci);
+    t.is(db, db);
 });
