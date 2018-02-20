632c8cf17500849535359a54caf396111b35c92f
astur <astur@yandex.ru>
2018-02-20 20:15:19 +0300

    more complicated test

--- test.js
+++ test.js
@@ -5,5 +5,8 @@ test('mongo-test-on-travis-ci', async t => {
     const _db = await db;
     await _db.collection('test').remove({});
     t.is(await _db.collection('test').count(), 0);
+    await _db.collection('test').insert({a: 1});
+    t.is(await _db.collection('test').count(), 1);
+    t.is((await _db.collection('test').find({a: 1}).toArray())[0].a, 1);
     await _db.close();
 });
