df98aee543cf3585b83b2d77b13e1fe9a44fd934
astur <astur@yandex.ru>
2018-02-20 18:23:25 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = () => {};
+const mongo = require('mongodb').MongoClient;
+const mongoString = process.env.MONGO_URI ||
+    'mongodb://localhost:27017/test';
+
+module.exports = mongo.connect(mongoString)
+    .catch(e => {
+        console.log(e.message);
+        process.exit(1);
+    });
