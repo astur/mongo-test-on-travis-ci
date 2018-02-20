b07020b8a385075769a2cdf16405d32663f0b0c4
astur <astur@yandex.ru>
2018-02-20 20:29:09 +0300

    mongo 2 or 3 compatible

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const mongoString = process.env.MONGO_URI ||
     'mongodb://localhost:27017/test';
 
 module.exports = mongo.connect(mongoString)
+    .then(client => {
+        if('databaseName' in client){
+            return client;
+        }
+        const db = client.db('test');
+        db.close = client.close.bind(client);
+        return db;
+    })
     .catch(e => {
         console.log(e.message);
         process.exit(1);
